93b9583bdaf0d6119c7c85a80a4e9386add720c3
AleksKirienko <aleks.Kirienko@bk.ru>
2020-07-27 01:52:04 +0700

    mvvm pattern

--- Weather/app/src/main/java/shift/cft/weather/presentation/ui/weather/list/WeatherActivity.kt
+++ Weather/app/src/main/java/shift/cft/weather/presentation/ui/weather/list/WeatherActivity.kt
@@ -2,42 +2,33 @@ package shift.cft.weather.presentation.ui.weather.list
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_weather.*
 import shift.cft.weather.R
 import shift.cft.weather.model.entity.Info
 import shift.cft.weather.presentation.ui.weather.CityDetailActivity
+import shift.cft.weather.presentation.viewmodel.weather.list.WeatherListViewModel
 
 class WeatherActivity : AppCompatActivity() {
 
-    private val citiesList = listOf(
-
-        Info("Барнаул", "Переменная облачность, +26°C"),
-        Info("Бердск", "Переменная облачность +22°C"),
-        Info("Воронеж", "Малооблачно +23°C"),
-        Info("Екатеринбург", "Пасмурно +22°C"),
-        Info("Казань", "Малооблачно +22°C"),
-        Info("Москва", "Местами грозы +17°C"),
-        Info("Новосибирск", "Преимущ. солнечно +23°C"),
-        Info("Омск", "Малооблачно +22°C"),
-        Info("Самара", "Облачно +22°C"),
-        Info("Санкт-Петербург", "Облачно +18°C"),
-        Info("Тольятти", "Малооблачно +25°C"),
-        Info("Тюмень", "Облачно +23°C"),
-        Info("Уфа", "Пасмурно, небольшой дождь +23°C"),
-        Info("Челябинск", "Облачно +25°C")
-    )
-
-    val adapter = WeatherListAdapter { model ->
-        val intent = Intent(this@WeatherActivity, CityDetailActivity::class.java)
-        intent.putExtra("City", model)
-        startActivity(intent)
+    private val viewModel: WeatherListViewModel by viewModels()
+    private val adapter = WeatherListAdapter { model ->
+        viewModel.citiesClicked(model)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+        viewModel.cities.observe(this, Observer { citiesList ->
+            setCitiesList(citiesList)
+        })
+
+        viewModel.cityClickedEvent.observe(this, Observer { weatherList ->
+            showWeatherDetails(weatherList)
+        })
 
         val layoutManager =
             LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
@@ -45,6 +36,16 @@ class WeatherActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = layoutManager
 
+
+    }
+
+    private fun setCitiesList(citiesList: List<Info>) {
         adapter.setWeatherList(citiesList)
+    }
+
+    private fun showWeatherDetails(model: Info) {
+        val intent = Intent(this@WeatherActivity, CityDetailActivity::class.java)
+        intent.putExtra("City", model)
+        startActivity(intent)
     }
 }
--- Weather/app/src/main/java/shift/cft/weather/presentation/viewmodel/weather/list/WeatherListViewModel.kt
+++ Weather/app/src/main/java/shift/cft/weather/presentation/viewmodel/weather/list/WeatherListViewModel.kt
@@ -1,37 +1,38 @@
 package shift.cft.weather.presentation.viewmodel.weather.list
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import shift.cft.weather.model.entity.Info
+import shift.cft.weather.presentation.viewmodel.SingleLiveEvent
 
-class WeatherListViewModel (){
+class WeatherListViewModel : ViewModel() {
 
     private val citiesList = listOf(
 
-        Info(
-            "Барнаул",
-            "Переменная облачность, +26°C"
-        ),
-        Info(
-            "Бердск",
-            "Переменная облачность +22°C"
-        ),
+        Info("Барнаул", "Переменная облачность, +26°C"),
+        Info("Бердск", "Переменная облачность +22°C"),
         Info("Воронеж", "Малооблачно +23°C"),
         Info("Екатеринбург", "Пасмурно +22°C"),
         Info("Казань", "Малооблачно +22°C"),
         Info("Москва", "Местами грозы +17°C"),
-        Info(
-            "Новосибирск",
-            "Преимущ. солнечно +23°C"
-        ),
+        Info("Новосибирск", "Преимущ. солнечно +23°C"),
         Info("Омск", "Малооблачно +22°C"),
         Info("Самара", "Облачно +22°C"),
         Info("Санкт-Петербург", "Облачно +18°C"),
         Info("Тольятти", "Малооблачно +25°C"),
         Info("Тюмень", "Облачно +23°C"),
-        Info(
-            "Уфа",
-            "Пасмурно, небольшой дождь +23°C"
-        ),
+        Info("Уфа", "Пасмурно, небольшой дождь +23°C"),
         Info("Челябинск", "Облачно +25°C")
     )
 
+    val cities = MutableLiveData<List<Info>>()
+    val cityClickedEvent = SingleLiveEvent<Info>()
+
+    init {
+        cities.value = citiesList
+    }
+
+    fun citiesClicked(model: Info) {
+        cityClickedEvent(model)
+    }
 }
