ae160d7fff2862e5a5b6cb77a29d72bc686ea8de
AleksKirienko <aleks.Kirienko@bk.ru>
2020-08-03 12:14:39 +0700

    Update migration file

--- server/src/repository/WeatherRepository.kt
+++ server/src/repository/WeatherRepository.kt
@@ -1,6 +1,9 @@
 package shift.cft.server.repository
 
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
+import shift.cft.common.CreateCityDto
 import shift.cft.server.db.dbQuery
 import shift.cft.server.db.table.Cities
 import shift.cft.server.db.table.toInfo
@@ -10,4 +13,22 @@ class WeatherRepository {
         dbQuery {
             Cities.selectAll().map { it.toInfo() }
         }
+
+    suspend fun add(createCityDto: CreateCityDto) {
+        dbQuery {
+            Cities.insert { insertStatement ->
+                insertStatement[title] = createCityDto.title
+                insertStatement[description] = createCityDto.description
+                insertStatement[temperature] = createCityDto.temperature
+            }
+        }
+    }
+
+    suspend fun delete(id: Long) {
+        dbQuery {
+            Cities.deleteWhere {
+                Cities.id.eq(id)
+            }
+        }
+    }
 }
